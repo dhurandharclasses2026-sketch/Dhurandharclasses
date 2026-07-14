beb481bda85d76999a09d901ce6fa8bb02653aba
dhurandharclasses2026-sketch <dhurandharclasses2026@gmail.com>
2026-07-14 21:51:26 +0530

    Create Script.js

---
+++ Script.js
@@ -0,0 +1,7 @@
+// Dhurandhar's Classes Website
+
+console.log("Welcome to Dhurandhar's Classes!");
+
+document.addEventListener("DOMContentLoaded", function () {
+    alert("Welcome to Dhurandhar's Classes 🎓");
+});
